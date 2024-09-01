60e0dd78dc5cd423c75543249c57411aab5ab720
phartex <fateruvictor96@gmail.com>
2024-09-01 19:44:26 +0100

    update

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -52,8 +52,11 @@ const Landing = ({ setSelectedPage }) => {
                     </p>
 
                     <p className="mt-10 mb-4 text-lg text-center md:text-start">
-                       I am a self taught frontend developer with over four years of active development, more actively in the financial technology industry,
+                       I am a self taught software enginer with over four years of active development across different industries(financial technology, software solutionsn and ERP)
                     </p>
+                    <p className="mt-10 mb-4 text-lg text-center md:text-start">
+                     I have experience building shippable products used by over 50,000 users daily with over 90% success rate.
+                     </p>
                 </motion.div>
 
                 <motion.div
